99cde60fa8c4c214d842e94b0c8c237caa74e64c
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2023-01-16 17:29:31 +0200

    added the permission to not POST in any profile except yours

--- profiles_feed/profilesapi/views.py
+++ profiles_feed/profilesapi/views.py
@@ -1,10 +1,14 @@
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 from .models import *
 from .serializers import *
+from .permissions import *
 # Create your views here.
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class=ProfileSerializer
     queryset=UserProfile.objects.all()
+    authentication_classes=(TokenAuthentication,)
+    permission_classes=(UpdateOwnProfile,)
