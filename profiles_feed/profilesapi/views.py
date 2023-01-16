031fefdd8d4aded29bcb1366d82f80177d8bf3b4
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2023-01-16 16:46:48 +0200

    added the viewset of userprofile

--- profiles_feed/profilesapi/views.py
+++ profiles_feed/profilesapi/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework import status
+from .models import *
+from .serializers import *
 # Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class=ProfileSerializer
+    queryset=UserProfile.objects.all()
