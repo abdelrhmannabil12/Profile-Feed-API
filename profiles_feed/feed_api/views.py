f46af4a719d2d7e07a997ddf552bbb98789ca615
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2023-01-17 14:17:23 +0200

    connected the urls to the UserProfile viewset

--- profiles_feed/feed_api/views.py
+++ profiles_feed/feed_api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework import filters
+from .models import *
+from .serializers import *
 
 # Create your views here.
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    authentication_classes=(TokenAuthentication,)
+    serializer_class=ProfileFeedItemSerializer
+    queryset=ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user_profile=self.request.user)
+        return 
