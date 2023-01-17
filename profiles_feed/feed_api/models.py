2166350bd6e4fa18b0be8b8330f744e5aa113781
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2023-01-17 13:41:25 +0200

    created the profile feed model and register it to the admin

--- profiles_feed/feed_api/models.py
+++ profiles_feed/feed_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class ProfileFeedItem(models.Model):
+    user_profile=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    status=models.CharField(max_length=200)
+    created_on=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.status
