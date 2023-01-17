98aea9b1356fdaf82dd8b7f3fcb1898566ca5bb7
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2023-01-17 17:11:51 +0200

    adding the permissions to view profile feeds

--- profiles_feed/feed_api/views.py
+++ profiles_feed/feed_api/views.py
@@ -3,18 +3,20 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from rest_framework import filters
 from .models import *
 from .serializers import *
+from .permissions import *
 
 # Create your views here.
 class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes=(TokenAuthentication,)
     serializer_class=ProfileFeedItemSerializer
     queryset=ProfileFeedItem.objects.all()
-
+    permission_classes=(UpdateOwnStatus,IsAuthenticated,)
     def perform_create(self, serializer):
         serializer.save(user_profile=self.request.user)
         return 
